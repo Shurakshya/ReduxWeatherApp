19d0a7f7de3fb58d1518489f2046ca88a6c7c688
Shurakshya Kharel <kharel.shurakshya@gmail.com>
2018-02-08 00:10:10 +0200

    Integration of google Map

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import GoogleMap from '../components/googleMap';
 import Chart from '../components/chart';
 
 class WeatherList extends Component{
@@ -8,9 +9,13 @@ class WeatherList extends Component{
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat } = cityData.city.coord;
+
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td>
+          <GoogleMap lat={lat} lon={lon} />
+        </td>
         <td>
           <Chart data={temps} color = {"orange"} units={"K"} />
         </td>
