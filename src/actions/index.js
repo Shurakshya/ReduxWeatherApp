4ee4ae1ef82852053ad30e75fa1dbc2f7c07342e
Shurakshya Kharel <kharel.shurakshya@gmail.com>
2018-02-07 23:37:55 +0200

    data forecast with chart

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import {FETCH_WEATHER} from "../constants";
+import {API_KEY} from "../env";
+
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type : FETCH_WEATHER,
+    payload : request
+  }
+
+}
